b6497d74c04568dfd79b35f8000b0c5a476606c6
Manasa651601 <ssatyasivamanasa18@gmail.com>
2024-11-15 20:36:30 +0530

    Update LoginSteps.cs

--- FrameworkLayer/StepDefinition/LoginSteps.cs
+++ FrameworkLayer/StepDefinition/LoginSteps.cs
@@ -1,23 +1,53 @@
 using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using PageObjectsLayer;
+using UtilityLayer;
 
-[Binding]
-public class LoginSteps
+namespace FrameworkLayer.StepDefinition
 {
-    [Given(@"I navigate to the login page")]
-    public void GivenINavigateToTheLoginPage()
+    [Binding]
+    public class LoginSteps
     {
-        // Navigate to login page
-    }
+        private IWebDriver _driver;
+        private LoginPage _loginPage;
 
-    [When(@"I login with username '(.*)' and password '(.*)'")]
-    public void WhenILoginWithUsernameAndPassword(string username, string password)
-    {
-        // Enter username and password and submit form
-    }
+        public LoginSteps(IWebDriver driver)
+        {
+            _driver = driver;
+            _loginPage = new LoginPage(_driver);
+        }
 
-    [Then(@"the login '(.*)' be successful")]
-    public void ThenTheLoginBeSuccessful(string result)
-    {
-        // Validate login result
+        [Given(@"I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [When(@"I enter username as '(.*)' and password as '(.*)'")]
+        public void WhenIEnterUsernameAsAndPasswordAs(string username, string password)
+        {
+            _loginPage.EnterUsername(username);
+            _loginPage.EnterPassword(password);
+        }
+
+        [When(@"I click on the login button")]
+        public void WhenIClickOnTheLoginButton()
+        {
+            _loginPage.ClickLoginButton();
+        }
+
+        [Then(@"I should be logged in successfully")]
+        public void ThenIShouldBeLoggedInSuccessfully()
+        {
+            Assert.IsTrue(_loginPage.IsLoggedInSuccessfully());
+        }
+
+        [Then(@"I should see the error message '(.*)'")]
+        public void ThenIShouldSeeTheErrorMessage(string expectedErrorMessage)
+        {
+            string actualErrorMessage = _loginPage.GetErrorMessage();
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage);
+        }
     }
 }
