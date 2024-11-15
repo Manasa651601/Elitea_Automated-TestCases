c0185caa382917d483cefb590b6cb706ad8afee3
Manasa651601 <ssatyasivamanasa18@gmail.com>
2024-11-15 20:31:38 +0530

    Create PageObjectsLayer/Locators.cs

---
+++ PageObjectsLayer/Locators.cs
@@ -0,0 +1,7 @@
+public static class Locators
+{
+    public static By UsernameField = By.Id("username");
+    public static By PasswordField = By.Id("password");
+    public static By LoginButton = By.Id("login");
+    public static By ErrorMessage = By.Id("error");
+}
